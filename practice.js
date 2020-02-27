de69389bb691e4ca6990b3ac7fd901ad237ae5a9
Maricruz Dias <maricruzdias@Maricruzs-MacBook-Air.local>
2020-02-26 20:31:19 -0600

    finished

--- practice.js
+++ practice.js
@@ -19,7 +19,10 @@ var arr = [10,20,30];
 
 //Code Here
 
+function first(arr) {
 
+  return arr[0];
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -34,7 +37,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+function last(arr) {
+  return arr [2];
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +54,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i]);
+  }
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -64,8 +73,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
-
+function reversedLooper(letters){
+  for(let i = letters.length; i>=1;i--){alert(i)}
+}
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
@@ -79,7 +89,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
+function evenFinder(nums){
+  let arr = [];
+   for (let i = 0; i <= nums.length; i++){
+    if (nums[i] % 2 === 0){
+      arr.push(nums[i]);
+    }
+  
+   } return arr; 
 
+  }; 
 
 
 
